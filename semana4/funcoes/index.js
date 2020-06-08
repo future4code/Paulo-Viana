d12b8a2ae9ab1f2f232af35b6cf4ea4a61bb1017
Paulo-Viana <paulodev1612@gmail.com>
2020-06-08 16:37:52 -0300

    Exercícios de funções em JS

---
+++ semana4/funcoes/index.js
@@ -0,0 +1,42 @@
+/* -- EXERCÍCIO 1 -- */
+
+/*Não sei se entende a questão, mas eu trocando os nomes da função
+ela vai dar erro, pois o parametro já é (quantidade);*/
+
+
+
+/* -- EXERCÍCIO 2 -- */
+/* A -
+Índice 0 = Darvas 
+Índice 2 = João
+Índice 3 = O resultado é Paulinha e não Paula. Por isso dá undefined
+
+B - 
+Daria sim se eu chamasse o código no console assim: console.log(funcao(arrayDeNumeros, 1));
+Troque todas as string por números e deu certo*/
+
+
+
+/* -- EXERCÍCIO 3 -- */
+/*Ele vai ficar repetindo o laço direto  0 + 0 = 1  e  1 + 1 = 1
+e vai acrescentar esse valores infinitamente. function infinito(array)*/
+
+
+
+/* -- EXERCÍCIO 4 -- */
+/* A - 
+const idadeDog = (anosHumanos) => {
+    return anosHumanos * 7
+}
+
+console.log(idadeDog(6))
+
+B - 
+let infor = (nome, idade, endereco,) => {
+    if (estudante === sim) {
+        console.log(infor('Paulo', 32, 'Vila Lobos, nº902',))
+    }
+}
+Tentei, tentei e tente e não deu certo!*/
+
+
