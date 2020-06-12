f9ccaf76456e46882965a66593e6758c40aa00ca
Paulo-Viana <paulodev1612@gmail.com>
2020-06-12 15:25:48 -0300

    Exercícios 1, 2, 3, 4 de Interpretação de Códigos

---
+++ semana4/lista-de-exercicios/index.js
@@ -0,0 +1,26 @@
+/* -- INTERPRETAÇÃO DE CÓDIGOS -- */
+/* 
+1 - Ela solicita ao usuário para digitar a cotação do dólar e retorna o valor convertido em R$100 * valor digitado
+
+2 - No caso ele vai passar por todos.
+No novoMontante ele corre até ações e multiplica o valor por 1.1.
+No segundoMontante ele corre até deafult e dá o alert de TIPO DE INVESTIMENTO INFORMADO INCORRETO
+
+3 - 
+1º console.log = Quantidade total de números 14
+2º console.log = array.1(só números pares) = 6 números
+3º console.log = array.1(só números ímpares) = 8 números
+
+4 - 
+- console.log(numero1) - ele vai percorrer todo o array e todos os números são menores do que o infinity,
+logo o resultado é o próprio infinity
+- console.log(numero2) - ele vai percorrer todo o array e vai pegar o maior número, que é 1590
+*/
+
+
+
+
+
+
+
+
